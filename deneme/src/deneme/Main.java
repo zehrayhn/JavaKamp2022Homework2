fdbe044cafdeaf3b75285fcfef6c19710cf04997
Zehra Ayhan <75533288+zehrayhn@users.noreply.github.com>
2022-07-29 01:29:56 +0300

    ödev1 (#1)
    * Create Main.java
    
    * Update Main.java
    
    * Update .gitignore

---
+++ deneme/src/deneme/Main.java
@@ -0,0 +1,15 @@
+package deneme;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("deneme");
+		System.out.println("deneme");
+		System.out.println("deneme");
+		System.out.println("deneme");
+		System.out.println("deneme");
+		System.out.println("denedhtgme");
+
+	}
+
+}
