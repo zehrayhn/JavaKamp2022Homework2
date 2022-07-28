f032781ffc8f157baf8194358aab07ccfc861fdf
Zehra Ayhan <zehrayhn@outlook.com>
2022-07-29 02:12:00 +0300

    Update Main.java

--- deneme/src/deneme/Main.java
+++ deneme/src/deneme/Main.java
@@ -5,9 +5,9 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("deneme");
 		System.out.println("deneme");
-		System.out.println("deneme");
-		System.out.println("deneme");
-		System.out.println("deneme");
+		System.out.println("deneme2");
+		System.out.println("deneme2");
+		System.out.println("deneme2");
 		System.out.println("denedhtgme");
 
 	}
