4021385c2e11f6f054c612e8420d6265047a51d2
Zehra Ayhan <zehrayhn@outlook.com>
2022-09-24 21:52:11 +0300

    first commit

---
+++ JavaKamp2022Odev2/src/ReCapDemo_Classes/Main.java
@@ -0,0 +1,13 @@
+package ReCapDemo_Classes;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Dörtİşlem dörtİşlem=new Dörtİşlem();
+		int sonuc=dörtİşlem.Topla(3,4);
+		System.out.println(sonuc);
+		
+
+	}
+
+}
